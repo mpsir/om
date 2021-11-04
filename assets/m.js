6543b29a8fb4d12bdbbb57198bcb328bd95475d9
mpsir <74041330+mpsir@users.noreply.github.com>
2021-11-04 11:32:17 +0530

    Update m.js

--- assets/m.js
+++ assets/m.js
@@ -1,1 +1,1 @@
-console.log("from m.js");var o={a:function(){console.log("a")},b:function(){console.log("b")}};export{o as default};
+console.log("aaaa");var o={a:function(){console.log("a")},b:function(){console.log("b")}};export{o as default};
