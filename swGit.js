a7c1350ebc69c671829647517b606f15cd91b1fc
mpsir <74041330+mpsir@users.noreply.github.com>
2021-11-07 18:59:05 +0530

    ram

---
+++ swGit.js
@@ -0,0 +1,67 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var w = 'https://mpsir.github.io/om'
+var urlsToCache = [
+  w + '/',
+  //'/styles/main.css',
+  //'/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+       
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
