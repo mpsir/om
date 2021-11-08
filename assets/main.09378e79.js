9b0263ea5c41dd361918aa0cf27415eb82b2b242
mpsir <74041330+mpsir@users.noreply.github.com>
2021-11-08 10:57:07 +0530

    ram

---
+++ assets/main.09378e79.js
@@ -0,0 +1,1 @@
+import{a as o}from"./m.e6bd3e44.js";console.log("from main.js");globalThis.v=o;console.log(1+1);console.log(1+1);console.log(1+1);$("body").append("ok5");
--- swLocal.js
+++ swLocal.js
@@ -1,66 +1,104 @@
-var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  //'/',
-  //'/styles/main.css',
-  //'/script/main.js'
+  '/favicon.ico',
+  '/App/App.html',
+  '/lib/typeface-roboto/index.min.css',
+  '/lib/@mdi/font/css/materialdesignicons.min2.css',
+  '/lib/vuetify/vuetify.min2.css',  
+  '/lib/codemirror/lib/codemirror.min.css',
+  '/lib/codemirror/addon/fold/foldgutter.min.css',
+  '/lib/codemirror/theme/ayu-dark.min.css',
+  '/lib/jq.js',
+  '/lib/vuetify/vue.min.js',
+  '/lib/sortable.js',
+  '/lib/vuedraggable.umd.js',
+  '/lib/vuetify/vuetify.min.js',
+  '/lib/beautify.min.js',
+  '/lib/bean.min.js',
+  '/lib/cod.js'
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+addEventListener('install', (event) => {
+  event.waitUntil(async function() {
+    const cache = await caches.open('static-v1');
+    await cache.addAll(urlsToCache);
+  }());
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-       
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(
-          function(response) {
-
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
+// See https://developers.google.com/web/updates/2017/02/navigation-preload#activating_navigation_preload
+addEventListener('activate', event => {
+  event.waitUntil(async function() {
+    // Feature-detect
+    if (self.registration.navigationPreload) {
+      // Enable navigation preloads!
+      await self.registration.navigationPreload.enable();
+    }
+  }());
 });
 
-self.addEventListener('activate', function(event) {
+addEventListener('fetch', (event) => {
+  const { request } = event;
 
-  var cacheAllowlist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+  // Always bypass for range requests, due to browser bugs
+  if (request.headers.has('range')) return;
+  event.respondWith(async function() {
+    // Try to get from the cache:
+    const cachedResponse = await caches.match(request);
+    if (cachedResponse) return cachedResponse;
 
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheAllowlist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+    try {
+      // See https://developers.google.com/web/updates/2017/02/navigation-preload#using_the_preloaded_response
+      const response = await event.preloadResponse;
+      if (response) return response;
+
+      // Otherwise, get from the network
+      return await fetch(request);
+    } catch (err) {
+      // If this was a navigation, show the offline page:
+      if (request.mode === 'navigate') {
+        return caches.match('/App/App.html');
+      }
+
+      // Otherwise throw
+      throw err;
+    }
+  }());
 });
+
+///////////////
+
+// console.log('from swLocal.js ok3');
+// var CACHE_NAME = 'my-site-cache-v2';
+
+
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+
+//         try {
+//         return fetch(event.request);
+//         } catch (error) {
+//           return caches.match('offline.html');
+//         }
+
+//       }
+//     )
+//   );
+// });
 
 
