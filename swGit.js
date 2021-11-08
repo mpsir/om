5c69d2fffc6ae12476a54b9d895701c0894ff705
mpsir <74041330+mpsir@users.noreply.github.com>
2021-11-08 11:38:28 +0530

    Update swGit.js

--- swGit.js
+++ swGit.js
@@ -1,21 +1,21 @@
 console.log('from git');
 var urlsToCache = [
-  'http://mpsir.github.io/om' + '/favicon.ico',
-  'http://mpsir.github.io/om' + '/App/App.html',
-  'http://mpsir.github.io/om' + '/lib/typeface-roboto/index.min.css',
-  'http://mpsir.github.io/om' + '/lib/@mdi/font/css/materialdesignicons.min2.css',
-  'http://mpsir.github.io/om' + '/lib/vuetify/vuetify.min2.css',  
-  'http://mpsir.github.io/om' + '/lib/codemirror/lib/codemirror.min.css',
-  'http://mpsir.github.io/om' + '/lib/codemirror/addon/fold/foldgutter.min.css',
-  'http://mpsir.github.io/om' + '/lib/codemirror/theme/ayu-dark.min.css',
-  'http://mpsir.github.io/om' + '/lib/jq.js',
-  'http://mpsir.github.io/om' + '/lib/vuetify/vue.min.js',
-  'http://mpsir.github.io/om' + '/lib/sortable.js',
-  'http://mpsir.github.io/om' + '/lib/vuedraggable.umd.js',
-  'http://mpsir.github.io/om' + '/lib/vuetify/vuetify.min.js',
-  'http://mpsir.github.io/om' + '/lib/beautify.min.js',
-  'http://mpsir.github.io/om' + '/lib/bean.min.js',
-  'http://mpsir.github.io/om' + '/lib/cod.js'
+  'https://mpsir.github.io/om' + '/favicon.ico',
+  'https://mpsir.github.io/om' + '/App/App.html',
+  'https://mpsir.github.io/om' + '/lib/typeface-roboto/index.min.css',
+  'https://mpsir.github.io/om' + '/lib/@mdi/font/css/materialdesignicons.min2.css',
+  'https://mpsir.github.io/om' + '/lib/vuetify/vuetify.min2.css',  
+  'https://mpsir.github.io/om' + '/lib/codemirror/lib/codemirror.min.css',
+  'https://mpsir.github.io/om' + '/lib/codemirror/addon/fold/foldgutter.min.css',
+  'https://mpsir.github.io/om' + '/lib/codemirror/theme/ayu-dark.min.css',
+  'https://mpsir.github.io/om' + '/lib/jq.js',
+  'https://mpsir.github.io/om' + '/lib/vuetify/vue.min.js',
+  'https://mpsir.github.io/om' + '/lib/sortable.js',
+  'https://mpsir.github.io/om' + '/lib/vuedraggable.umd.js',
+  'https://mpsir.github.io/om' + '/lib/vuetify/vuetify.min.js',
+  'https://mpsir.github.io/om' + '/lib/beautify.min.js',
+  'https://mpsir.github.io/om' + '/lib/bean.min.js',
+  'https://mpsir.github.io/om' + '/lib/cod.js'
 ];
 
 
