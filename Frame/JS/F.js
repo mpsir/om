7d1b44e90a3d793dd3d23d01270a54b1ca5cb565
mpsir <74041330+mpsir@users.noreply.github.com>
2023-03-19 19:11:58 +0530

    a

--- Frame/JS/F.js
+++ Frame/JS/F.js
@@ -13,23 +13,22 @@ function create_script(file_path, isAsync, callback) {
 	};
 	document.body.appendChild(script);
 }
-g.JSONF = JSONF;
 g.f = {
 	// window.location.hostname.search("github.io")
-	c: async function() {},
-	IsGithubHosted: function() {
+	c: async function () { },
+	IsGithubHosted: function () {
 		if (window.location.hostname.search('github.io') == -1) {
 			return false;
 		}
 		return true;
 	},
-	add2:function() {
+	add2: function () {
 		g.db.friends.add({
 			name: 'a2',
 			age: 566
 		});
 	},
-	addDB:function() {
+	addDB: function () {
 		g.db = new Dexie('myDatabase');
 		db.version(1).stores({
 			funs: '++id, name, value',
@@ -46,26 +45,26 @@ g.f = {
 		// 	age: 5,
 		//   });
 	},
-	GetHighestZ:function() {
+	GetHighestZ: function () {
 		return Math.max.apply(
 			null,
-			$.map($('body *'), function(e, n) {
+			$.map($('body *'), function (e, n) {
 				if ($(e).css('position') != 'static')
 					return parseInt($(e).css('z-index')) || 1;
 			})
 		);
 	},
-	start: async function() {
+	start: async function () {
 		var script_path = '/Frame/JS/F.js';
 		if (g.f.IsGithubHosted()) {
 			script_path = 'https://mpsir.github.io/om' + script_path;
 		}
-		create_script(script_path, (isAsync = false), function() {});
+		create_script(script_path, (isAsync = false), function () { });
 
 		g.f.addDB();
 		g.f.Boot();
 	},
-	GetPageName:function() {
+	GetPageName: function () {
 		const queryString = window.location.search;
 		if (queryString == '') {
 			return 'home';
@@ -79,20 +78,20 @@ g.f = {
 		}
 		return 'home';
 	},
-	parse:function(s) {
-		return JSONF.parse(s);
+	parse: function (s) {
+		return JSON.parse(s);
 	},
-	string:function (s) {
-		return JSONF.stringify(s);
+	string: function (s) {
+		return JSON.stringify(s);
 	},
-	pstring: function(data) {
-		return JSONF.parse(JSONF.stringify(data));
+	pstring: function (data) {
+		return JSON.parse(JSON.stringify(data));
 	},
-	AppReBoot:function() {
+	AppReBoot: function () {
 		g.App.unmount();
 		Boot();
 	},
-	GetPageObject:function(PageName) {
+	GetPageObject: function (PageName) {
 		var FoundPage = g.d.Pages.find(p => {
 			return p.name == PageName;
 		});
@@ -104,40 +103,23 @@ g.f = {
 			});
 		}
 	},
-	ArrayUp:function(Arr, INo) {},
-	ArrayDn:function(Arr, INo) {},
-	ArrayDup:function(Arr, INo) {},
-	ArrayDel:function(Arr, INo) {},
-	ArrayMoveTo:function(Arr, INo) {},
-	ArrToObject:function() {},
-	ObjectToArr:function(obj1) {
+	ArrayUp: function (Arr, INo) { },
+	ArrayDn: function (Arr, INo) { },
+	ArrayDup: function (Arr, INo) { },
+	ArrayDel: function (Arr, INo) { },
+	ArrayMoveTo: function (Arr, INo) { },
+	ArrToObject: function () { },
+	ObjectToArr: function (obj1) {
 		var r = Object.entries(obj1);
 		return r;
 	},
-	// create_script(file_path, isAsync, callback) {
-	// 	const script = document.createElement('script');
-	// 	script.setAttribute('src', file_path);
-	// 	script.setAttribute('async', isAsync + '');
-	// 	script.onload = function handleScriptLoaded() {
-	// 		if (callback != null) {
-	// 			callback();
-	// 		}
-	// 	};
-	// 	script.onerror = function handleScriptError() {
-	// 		console.log('error loading script', file_path);
-	// 	};
-	// 	document.body.appendChild(script);
-	// },
-	
-	CreateApp:function(App, DomTarget) {
-		//console.log(App, 'App');
+	CreateApp: function (App, DomTarget) {
+		console.log('App', App);
 		var a = Vue.createApp(App);
 		{
 			a.use(window['VueShortkey']);
 			a.use(Quasar);
-			a.use(vdrag, {
-				// global configuration
-			});
+			a.use(vdrag, {});
 			a.use(window['v-resizable']);
 		}
 		{
@@ -146,34 +128,24 @@ g.f = {
 				// called before bound element's attributes
 				// or event listeners are applied
 				created(el, binding, vnode, prevVnode) {
-					//console.log(binding);
-					// console.log('vnode', vnode);
 					// g.a = vnode
-					// console.log('vnode', JSON.stringify(vnode));
 					// see below for details on arguments
 				},
 				// called right before the element is inserted into the DOM.
-				beforeMount(el, binding, vnode, prevVnode) {
-					//console.log(binding);
-				},
+				beforeMount(el, binding, vnode, prevVnode) { },
 				// called when the bound element's parent component
 				// and all its children are mounted.
-				mounted(el, binding, vnode, prevVnode) {},
+				mounted(el, binding, vnode, prevVnode) { },
 				// called before the parent component is updated
-				beforeUpdate(el, binding, vnode, prevVnode) {
-					console.log(binding);
-				},
+				beforeUpdate(el, binding, vnode, prevVnode) { },
 				// called after the parent component and
 				// all of its children have updated
-				updated(el, binding, vnode, prevVnode) {
-					console.log(binding);
-				},
+				updated(el, binding, vnode, prevVnode) { },
 				// called before the parent component is unmounted
-				beforeUnmount(el, binding, vnode, prevVnode) {},
+				beforeUnmount(el, binding, vnode, prevVnode) { },
 				// called when the parent component is unmounted
-				unmounted(el, binding, vnode, prevVnode) {}
+				unmounted(el, binding, vnode, prevVnode) { }
 			});
-			
 		}
 		{
 			// comps
@@ -188,101 +160,91 @@ g.f = {
 				d1 {{ d1 }}
 				</div>`
 			});
-			a.component(
-				'm-edit-funs', {
-					setup:function() {
-						return {
-							funs: VueUse.useObservable(
-								Dexie.liveQuery(() => db.funs.toArray())
-							)
-						};
-					},
-					template: `
-					<div>
-					{{ funs }}
+			a.component('m-edit-funs', {
+				setup: function (props, { attrs, slots, emit, expose }) {
+					return {
+						funs: VueUse.useObservable(
+							Dexie.liveQuery(() => db.funs.toArray())
+						),
+						g: Vue.computed(() => g)
+					};
+				},
+				template: `
+					<div v-if="false">
+						<m-editor format_on_start="true" language='json' :val="g.f.string(g.f)"></m-editor>
 					</div>
+					<div v-if="true" v-for="(value, key, index) in g.f">
+					<p>
+					<h6>{{ key }}</h6>
+					{{ typeof value }}, <br/>
+					{{ typeof g.JSON.parse(value) }}
+					<m-editor v-if="false" format_on_start="true" language='javascript' 
+						:val="g.f.string(value)"></m-editor>
 
-					<div>
-						<m-editor></m-editor>
+					</p>
+						<m-editor v-if="false" format_on_start="true" language='json' :val="g.f.string(g.f)"></m-editor>
 					</div>
 					`
-				}
-			)
+			});
 			a.component('m-editor', {
-				template:`
-				<div style="border:1px solid black">
-					<div ref="m_editor" style="min-height:28px;" class="">
+				setup: function (props, { attrs, slots, emit, expose }) {
+					return {
+						editor: {},
+						g: Vue.computed(() => g),
+						update_editor: function () {
+							const contentHeight = this.editor.getModel().getLineCount() * 19;
+							$(this.$refs.m_editor).css('height', contentHeight + 'px');
+							this.editor.layout();
+						}
+					};
+				},
+				props: {
+					val: { type: String, required: true }
+				},
+				template: `
+				<div style="border:1px dashed darkgrey">
+					<div ref="m_editor" style="min-height:28px;">
 					</div>
 				</div>
 				`,
 				mounted() {
-					console.log(this.$refs.m_editor)
-					const text = `function hello() {
-						alert('Hello world!');
-					}`;
-
-					var editor = g.monaco.editor.create(
-						this.$refs.m_editor, 
-					{
-						value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-						language: 'javascript'
+					this.editor = g.monaco.editor.create(this.$refs.m_editor, {
+						value: this.val,
+						language: this.$attrs.language || 'json',
+						readOnly: false,
+						minimap: { enabled: false }
 					});
-
-					setTimeout(() => {
-						const contentHeight = editor.getModel().getLineCount() * 19 ;
-						console.log(contentHeight);
-						$(this.$refs.m_editor).css('height', (contentHeight + 'px') )
-						editor.layout()
-					}, 1000);
-
-					editor.getModel().onDidChangeContent((event) => {
-						const contentHeight = editor.getModel().getLineCount() * 19 ;
-						console.log(contentHeight);
-						$(this.$refs.m_editor).css('height', (contentHeight + 'px') )
-						editor.layout()
-					  });
-
-					// g.require(['vs/editor/editor.main'], function () {
-					// 	var editor = monaco.editor.create(this.$refs.m_editor, {
-					// 		value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-					// 		language: 'javascript'
-					// 	});
-					// });
-					
-					// Hover on each property to see its docs!
-					// monaco.editor.create(this.$refs.m_editor, {
-					// 	value: text,
-					// 	language: "javascript",
-					// 	automaticLayout: true,
-					// });
-				  }
-			})
+					setTimeout(() => { this.update_editor() }, 100);
+					if (JSON.parse(this.$attrs.format_on_start || true)) {
+						setTimeout(() => { this.editor.getAction('editor.action.formatDocument').run() }, 500);
+					}
+					this.editor.getModel().onDidChangeContent(event => { this.update_editor() });
+				}
+			});
 		}
 		return a.mount(DomTarget);
 	},
-	BootEditor:function() {
+	BootEditor: function () {
 		g.AppEditor = g.f.CreateApp(
 			g.f.pstring(g.d.EditorObj.Vue),
 			'#editor-dom-wrapper'
 		);
 	},
-	Boot:function() {
+	Boot: function () {
 		var PageObject = g.f.GetPageObject(g.f.GetPageName());
 		if (PageObject) {
 			PageObject.Vue.template = g.f.ResolveTemplate(PageObject.Vue.template);
-			//console.log('PageObject', PageObject);
-			// console.log('template', PageObject.Vue.template);
+
 			g.App = g.f.CreateApp(g.f.pstring(PageObject.Vue), '#page-dom-wrapper');
 		} else {
 			console.log('Page Not Found: ', g.f.GetPageName());
 		}
 		//g.f.BootEditor();
 	},
-	ResolveTemplate:function(template) {
+	ResolveTemplate: function (template) {
 		if (typeof template == 'string') {
 			return template;
 		}
-		//console.log('template', template);
 		return template;
 	}
 };
@@ -298,7 +260,7 @@ g.d = {
 			name: 'home',
 			gcomps: [],
 			Vue: {
-				setup:function() {
+				setup: function (props, { attrs, slots, emit, expose }) {
 					const leftDrawerOpen = Vue.ref(false);
 					const rightDrawerOpen = Vue.ref(false);
 					return {
@@ -348,12 +310,9 @@ g.d = {
       </q-footer>
     </q-layout>      
         `,
-				created:function() {
-					// console.log(55);
-					//console.log(this.$toast.add)
-				},
+				created: function () { },
 				computed: {
-					g:function() {
+					g: function () {
 						return globalThis;
 					}
 				}
@@ -363,7 +322,7 @@ g.d = {
 			name: 'admin',
 			gcomps: [],
 			Vue: {
-				setup:function() {
+				setup: function (props, { attrs, slots, emit, expose }) {
 					return {
 						tab: Vue.ref('Funs')
 					};
@@ -452,18 +411,13 @@ g.d = {
 				  </q-page-container>
 				</q-layout>
         `,
-				created:function() {
-					// console.log(55);
-					//console.log(this.$toast.add)
-				},
+				created: function () { },
 				computed: {
-					g:function() {
+					g: function () {
 						return globalThis;
 					}
 				},
-				components: {
-					
-				}
+				components: {}
 			}
 		}
 	],
@@ -474,15 +428,13 @@ g.d = {
 		name: 'home',
 		gcomps: [],
 		Vue: {
-			data:function() {
+			data: function () {
 				return {};
 			},
 			methods: {
-				print:function(val) {
-					console.log(val);
-				}
+				print: function (val) { }
 			},
-			setup:function() {
+			setup: function (props, { attrs, slots, emit, expose }) {
 				return {
 					isB: true,
 					x,
@@ -508,9 +460,9 @@ g.d = {
     
     
    `,
-			created:function() {},
+			created: function () { },
 			computed: {
-				g:function() {
+				g: function () {
 					return globalThis;
 				}
 			}
