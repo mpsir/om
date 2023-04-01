02d82edb88eb38d6ffc2ce9aaa10bfce8eb25998
mpsir <74041330+mpsir@users.noreply.github.com>
2023-04-01 12:32:38 +0530

    a

--- Frame/JS/App.js
+++ Frame/JS/App.js
@@ -1,3 +1,33 @@
-g.start = function(){
-    $("body").append("ok")
+g.start = async function () {
+    $("body").append("started")
+    // if (navigator.platform.search("Win") == "-1") {
+    //     add_script('https://mpsir.github.io/om/Lib/eruda.js')
+    //     setTimeout(() => {
+    //         eruda.init()
+    //     }, 2000);
+    // }
+}
+
+function add_script(address) {
+    const script = document.createElement('script');
+
+    script.setAttribute(
+        'src',
+        address,
+    );
+
+    script.setAttribute('async', '');
+
+    script.onload = function handleScriptLoaded() {
+        console.log('script has loaded');
+        $("body").append("script added")
+
+    };
+
+    script.onerror = function handleScriptError() {
+        console.log('error loading script');
+    };
+
+    document.head.appendChild(script);
+
 }
