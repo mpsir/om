b7f80509efe1d3e400addc48e2cec37c2043466b
mpsir <74041330+mpsir@users.noreply.github.com>
2023-04-03 16:29:02 +0530

    a

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var fs = require('fs');
-const path = require('path');
-// console.log(555);
 eval(fs.readFileSync(__dirname + '/DB/d.js').toString());
 eval(fs.readFileSync(__dirname + '/DB/r.js').toString());
 eval(fs.readFileSync(__dirname + '/DB/f.js').toString());
 eval(fs.readFileSync(__dirname + '/DB/App.js').toString());
 console.log(555);
-// console.log(f);
